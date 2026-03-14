42015a02fa08cd6ff874c7d9b72312bc66912e8e
EvenG1O <raul.de@uabc.edu.mx>
2026-03-14 16:11:10 -0700

    875

---
+++ 875.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+int minTime(int mid, vector<int> &piles) {
+  long long  amount{};
+
+  for (int x : piles) {
+
+    amount += ((long long)x + mid - 1) / mid;
+  }
+
+  return amount;
+}
+
+int minEatingSpeed(vector<int> &piles, int h) {
+
+  int max = piles.back();
+  int ans = piles.back();
+
+
+
+  int l = 1;
+  int r = *max_element(piles.begin(), piles.end());
+
+  while (l <= r) {
+    int mid = l + (r - l) / 2;
+
+    if (minTime(mid, piles) <= h) {
+      ans = mid;
+      r = mid - 1;
+    } else {
+      l = mid + 1;
+    }
+  }
+
+  return ans;
+}
+
+int main() {
+
+  vector<int> temp = {30, 11, 23, 4, 20};
+
+  int ans = minEatingSpeed(temp, 6);
+
+  cout << ans;
+
+  return 0;
+}
