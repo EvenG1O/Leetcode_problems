89314f70d1751e8a6fc64ce21a7ae9d7696de927
EvenG1O <raul.de@uabc.edu.mx>
2026-05-08 17:21:28 -0700

    209

---
+++ 209.cpp
@@ -0,0 +1,43 @@
+
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int minSubArrayLen(int target, vector<int> &nums) {
+
+    int l = 0;
+
+    int currentSum{};
+
+    int minWindowSize = INT_MAX;
+
+    for (int r = 0; r < nums.size(); r++) {
+      currentSum += nums[r];
+      while (target <= currentSum) {
+        minWindowSize = min(minWindowSize, r - l + 1);
+        currentSum -= nums[l];
+        l++;
+      }
+    }
+
+    return minWindowSize == INT_MAX ? 0 : minWindowSize;
+  }
+};
+
+int main() {
+
+  Solution s;
+
+  vector<int> nums = {1,1,1,1,1,1,1,1};
+
+  int ans = s.minSubArrayLen(11, nums);
+
+  cout << ans << endl;
+
+  return 0;
+}
