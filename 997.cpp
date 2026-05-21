24a5d3d0886de73acc8f1627f6a7a3b32519111c
EvenG1O <raul.de@uabc.edu.mx>
2026-05-20 17:54:31 -0700

    997

---
+++ 997.cpp
@@ -0,0 +1,45 @@
+#include <ios>
+#include <iostream>
+#include <set>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+
+public:
+  int findJudge(int n, vector<vector<int>> &trust) {
+
+    vector<int> inc(n + 1, 0), out(n + 1, 0);
+
+    for (int i{}; i < trust.size(); i++) {
+
+      out[trust[i][0]]++;
+      inc[trust[i][1]]++;
+    }
+
+    for (int i =1; i <= n; i++) {
+
+      if (inc[i] == n - 1 && out[i] == 0)
+        return i;
+    }
+
+    return -1;
+  }
+};
+
+int main() {
+
+  Solution s;
+
+  int n = 1;
+  vector<vector<int>> trust = {};
+
+  int ans = s.findJudge(n, trust);
+
+  cout << ans <<endl;
+
+  return 0;
+}
