102f36669948ef938a9826f4f578ec70e0909da9
EvenG1O <raul.de@uabc.edu.mx>
2026-05-28 14:27:56 -0700

    169

---
+++ 169.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+
+using  namespace std;
+
+
+class Solution{
+    public:
+    unordered_map<int, int> m;
+    
+
+
+    int  majorityElement(vector<int> &nums){
+
+        int ans{};
+
+        for (int x : nums) {
+            m[x]++;
+ 
+            if((nums.size() / 2) < m[x]){
+                ans =x;
+                break;
+            }
+        }
+
+        return ans;
+        
+
+
+    }
+
+
+
+
+
+};
+
+
+
+
+int main()
+{
+    Solution s;
+
+    vector<int> test = {3,2,3};
+
+    int ans = s.majorityElement(test);
+
+
+    cout<<ans<<endl;
+
+
+
+    return 0;
+}
+
