ac4cc83bacfb2c55b8253c299cb1c0ac7ca743c0
EvenG1O <raul.de@uabc.edu.mx>
2026-05-29 11:24:28 -0700

    994

--- 169.cpp
+++ 169.cpp
@@ -1,59 +1,38 @@
-#include <algorithm>
 #include <iostream>
 #include <unordered_map>
 #include <vector>
 
+using namespace std;
 
-using  namespace std;
+class Solution {
+public:
+  unordered_map<int, int> m;
 
+  int majorityElement(vector<int> &nums) {
 
-class Solution{
-    public:
-    unordered_map<int, int> m;
-    
+    int ans{};
 
+    for (int x : nums) {
+      m[x]++;
 
-    int  majorityElement(vector<int> &nums){
-
-        int ans{};
-
-        for (int x : nums) {
-            m[x]++;
- 
-            if((nums.size() / 2) < m[x]){
-                ans =x;
-                break;
-            }
-        }
-
-        return ans;
-        
-
-
+      if ((nums.size() / 2) < m[x]) {
+        ans = x;
+        break;
+      }
     }
 
-
-
-
-
+    return ans;
+  }
 };
 
+int main() {
+  Solution s;
 
+  vector<int> test = {3, 2, 3};
 
+  int ans = s.majorityElement(test);
 
-int main()
-{
-    Solution s;
+  cout << ans << endl;
 
-    vector<int> test = {3,2,3};
-
-    int ans = s.majorityElement(test);
-
-
-    cout<<ans<<endl;
-
-
-
-    return 0;
+  return 0;
 }
-
---
+++ 994.cpp
@@ -0,0 +1,93 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+
+public:
+  set<pair<int, int>> visited;
+  int numRows{};
+  int numColumns{};
+  queue<pair<int, int>> q;
+
+  int ans{};
+
+  void dfs(vector<vector<int>> &grid, int c, int r) {
+
+    if (r > numRows - 1 || r < 0)
+      return;
+
+    if (c > numColumns - 1 || c < 0)
+      return;
+
+    if (visited.count({c, r}))
+      return;
+
+    visited.insert({c, r});
+
+    if (grid[c][r] == 0)
+      return;
+
+    q.push({c + 1, r});
+    q.push({c - 1, r});
+
+    q.push({c, r + 1});
+
+    q.push({c, r - 1});
+  }
+
+  int orangesRotting(vector<vector<int>> &grid) {
+
+    numColumns = grid.size();
+    numRows = grid[0].size();
+
+    for (int i{}; i < grid.size(); i++) {
+      for (int j{}; j < grid[0].size(); j++) {
+
+        if (grid[i][j] == 2)
+          q.push({i, j});
+      }
+    }
+
+    while (!q.empty()) {
+      int s = q.size();
+
+      for (int i{}; s > i; i++) {
+        dfs(grid, q.front().first, q.front().second);
+
+        q.pop();
+      }
+
+      if (!q.empty()) ans++;
+    }
+
+    for (int i{}; i < grid.size(); i++) {
+      for (int j{}; j < grid[0].size(); j++) {
+
+        if (grid[i][j] == 1 && !visited.count({i,j}))
+          return -1;
+      }
+    }
+
+
+    return ans > 0 ? ans - 1 : 0;
+  }
+};
+
+int main() {
+
+  Solution s;
+
+  std::vector<std::vector<int>> grid = {{2, 1, 1}, {1, 1, 0}, {0, 1, 1}};
+
+  int ans = s.orangesRotting(grid);
+
+  cout << ans << endl;
+
+  return 0;
+}
