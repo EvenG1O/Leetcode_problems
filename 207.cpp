dc0c3051a085276970b5491c33f7a22f681231d3
EvenG1O <raul.de@uabc.edu.mx>
2026-06-29 17:03:54 -0700

    207

---
+++ 207.cpp
@@ -0,0 +1,66 @@
+#include <ios>
+#include <iostream>
+#include <set>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  set<int> s;
+
+  unordered_map<int, vector<int>> m;
+
+  bool dfs(int courseNumber, const vector<vector<int>> &prerequesites,
+           set<int> &visitSet) {
+
+    if (s.count(courseNumber))
+      return true;
+    if (visitSet.count(courseNumber) == true)
+      return false;
+
+    visitSet.insert(courseNumber);
+
+    for (int i : m[courseNumber]) {
+
+      if (dfs(i, prerequesites, visitSet) == false) {
+        return false;
+      } else {
+        s.insert(i);
+      }
+    }
+
+    visitSet.erase(courseNumber);
+
+    return true;
+  }
+
+  bool canFinish(int numCourses, vector<vector<int>> &prerequesites) {
+
+    for (vector<int> x : prerequesites) {
+
+      m[x[0]].push_back(x[1]);
+    }
+
+    for (int i{}; i < numCourses; i++) {
+      set<int> vis;
+      if (dfs(i, prerequesites, vis) == false)
+        return false;
+    }
+
+    return true;
+  }
+};
+
+int main() {
+
+  Solution s;
+
+  vector<vector<int>> prerequisites = {{1, 0}, {0, 1}};
+
+  bool ans = s.canFinish(2, prerequisites);
+
+  cout << boolalpha << ans << endl;
+
+  return 0;
+}
